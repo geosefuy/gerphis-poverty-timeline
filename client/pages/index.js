9bf8d3cd3d87377b88ccccd4edf374bbbac1702c
Carlos Shi <carlos_shi@dlsu.edu.ph>
2022-06-17 21:51:52 +0800

    Added example

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,47 @@
+import { Chrono } from "react-chrono";
+
 function Home() {
+  const items = [
+    {
+      title: "1940",
+    },
+    {
+      title: "1941",
+    },
+    {
+      title: "1942",
+    },
+    {
+      title: "1943",
+    },
+    {
+      title: "1944",
+    },
+    {
+      title: "1945",
+    },
+    {
+      title: "1946",
+    },
+  ];
+
   return (
-    <div>
-      placeholder
+    <div style={{ width: "100vw", height: "600px" }}>
+      <Chrono
+        items={items}
+        mode="HORIZONTAL"
+        disableAutoScrollOnClick={true}
+        hideControls={true}
+      >
+        <div>
+          <img src="https://randomwordgenerator.com/img/picture-generator/50e9d4414851b10ff3d8992cc12c30771037dbf85254794e732873dc954f_640.jpg" />
+          <p>
+            Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum.
+            Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum. Lorem Ipsum.
+            Lorem Ipsum. Lorem Ipsum.{" "}
+          </p>
+        </div>
+      </Chrono>
     </div>
   );
 }
